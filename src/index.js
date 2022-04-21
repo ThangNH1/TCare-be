ef5455b68082a3310e9d2c9bd4d38433763c7674
Bee Nguyen <bee.nguyen@techvify.com.vn>
2022-04-21 19:00:40 +0700

    connect db?

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine"
 import initWebRouters from "./route/web"
-// import connectDB from "./config/connectDB"
+import connectDB from "./config/connectDB"
 require("dotenv").config()
 
 let app = express()
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 viewEngine(app)
 initWebRouters(app)
 
-// connectDB()
+connectDB()
 
 let port = process.env.PORT || 1234
 
